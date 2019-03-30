297583755365d33c5f01d3b45f7d4aafed9b9cec
Võ Thành Tài <taivtse@gmail.com>
2019-03-30 22:43:00 +0700

    Routing for login and container page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {ContainerComponent} from './container/container.component';
+import {CommonModule} from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent, pathMatch: 'full'},
+  {path: 'chat/:username', component: ContainerComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), CommonModule],
+  exports: [RouterModule],
+  declarations: [
+    LoginComponent,
+    ContainerComponent
+  ]
 })
 export class AppRoutingModule { }
